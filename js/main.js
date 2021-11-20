4a9a77d7c3bdf454f7ef9fa263245965b04406ca
VadimTs12 <67315881+VadimTs12@users.noreply.github.com>
2021-11-20 19:33:09 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,67 +1,181 @@
 
+const API = 'https://raw.githubusercontent.com/VadimTs12/API-products/master/online-shop.json'
 
-class ProductsList {
-    constructor(container = '.products') {
+class List {
+    constructor(url, container) {
+        this.url = url;
         this.container = container;
         this.products = [];
-        this.allProducts = [];
-        this._fetchProducts();
+        this.allProducts = []; 
+        this.getProd()
     }
 
-    _fetchProducts() {
-        this.products = [
-            { id: 1, title: 'Notebook', price: 2000, img: 'img/notebook.png' },
-            { id: 2, title: 'Mouse', price: 20, img: 'img/mouse.png' },
-            { id: 3, title: 'Keyboard', price: 200, img: 'img/keyboard.png' },
-            { id: 4, title: 'Gamepad', price: 50, img: 'img/gamepad.png' },
-        ];
-    }
-    render() {
-        const block = document.querySelector(this.container);
-        for (let product of this.products) {
-            const productObj = new ProductItem(product);
-            this.allProducts.push(productObj);
-            block.insertAdjacentHTML('beforeend', productObj.render())
+    async getJson() {
+        try {
+            const result = await fetch(this.url);
+            return await result.json();
+        } catch (error) {
+            console.log(error);
         }
     }
-    getSum() {
-        let res = this.allProducts.reduce((s, item) => s + item.price, 0);
-        const block = document.querySelector(this.container);
-        block.insertAdjacentHTML('afterend', `<div class="summ"><b>Сумма всех товаров: ${res}</b></div>`);
+
+    getProd() {
+         this.getJson()
+               .then(data => {           
+               this.products = [...data];  
+                this.renderList()
+            })
+    }
+
+    renderList() {
+        this.products.forEach(product => {
+            this.allProducts.push(new Item(product));
+            document.querySelector(this.container).insertAdjacentHTML('beforeend', new Item(product).renderItem())
+        })
     }
 }
 
-
-class ProductItem {
+class Item {
     constructor(product) {
         this.title = product.title;
         this.price = product.price;
         this.id = product.id;
         this.img = product.img;
-
+        this.quantity = product.quantity
     }
-
-    render() {
+    renderItem() {
         return `<div class="product-item">
                 <h3>${this.title}</h3>
                 <img src="${this.img}"
                 <p>${this.price}</p>
-                <button class="buy-btn">Купить</button>
+                <button class="buy-btn" 
+                data-id="${this.id}"
+                    data-name="${this.title}"
+                    data-price="${this.price}"
+                    data-image="${this.img}"
+                >Купить</button>
             </div>`
     }
 }
 
-let list = new ProductsList();
-list.render();
-list.getSum();
 
-class Card {
-    add() {}
-    remove() {}
-    change() {}
-    render() {}
+class ProductsList extends List {
+    constructor(cart, url = API, container = '.products') {
+        super(url, container) 
+        this.cart = cart;
+        this._init() 
+    }
+    _init() {
+        document.querySelector(this.container).addEventListener('click', e => {
+            if (e.target.classList.contains('buy-btn')) {
+                this.cart.addProduct(e.target);
+            }
+        });
+    }
+    
 }
 
-class ElemCard {
-    render() {}
+class CartList extends List{
+    constructor(url = API, container = '.cart-block') {
+        super(url, container);
+        this._clickCart();
+    }
+    _clickCart() {
+    document.querySelector('.btn-cart').addEventListener('click', () => {
+        document.querySelector(this.container).classList.toggle('invisible');
+    });
+        document.querySelector(this.container).addEventListener('click', e => {
+            if (e.target.classList.contains('del-btn')) {
+                this.removeProduct(e.target);
+            }
+            if (e.target.classList.contains('add-btn')) {
+                this.addProduct(e.target);
+            }
+        })
+        
+    }
+    addProduct(element) {
+        this.getJson() 
+        .then(data => {
+            let productId = +element.dataset['id'];
+            let find = this.allProducts.find(product => product.id === productId);
+            if (find) {
+                find.quantity++;
+                this._updateCart(find);
+            }
+            else {
+                    this.products = [{
+                    id: +element.dataset['id'],
+                    title: element.dataset['name'],
+                    price: +element.dataset['price'],
+                    img: element.dataset['image'],
+                    quantity: 1
+                }];
+                this.renderList();
+            }
+        })
+    }
+    _updateCart(product) {
+        let block = document.querySelector(`.cart-item[data-id="${product.id}"]`);
+        block.querySelector('.product-quantity').textContent = `Quantity: ${product.quantity}`;
+        block.querySelector('.product-price').textContent = `$${product.quantity * product.price}`;
+    }
+
+    removeProduct(element) {
+      this.getJson() 
+      .then(data => {
+          let productId = +element.dataset['id'];          
+          let find = this.allProducts.find(product => product.id === productId);
+          if (find.quantity > 1) {
+              find.quantity--;
+              this._updateCart(find);
+          }
+          else {
+              this.allProducts.splice(this.allProducts.indexOf(find), 1);
+              document.querySelector(`.cart-item[data-id="${productId}"]`).remove();
+          }
+      })
+    }
+    renderList() {          
+           this.products.map(product => {
+                this.allProducts.push(new CartItem(product));
+                document.querySelector(this.container).insertAdjacentHTML('beforeend', new CartItem(product).renderItem())
+            })
+        }
+
+    getSum() {
+        let res = this.products.reduce((s, item) => s + item.price, 0);
+        document.querySelector(this.container).insertAdjacentHTML('beforeEnd', `
+        <div class="summ"><b>Сумма всех товаров: ${res}</b></div>`
+        );
+    }
+
 }
+
+
+class CartItem extends Item {
+    constructor(product) {
+        super(product);
+    }
+    renderItem() {
+        return `<div class="cart-item" data-id="${this.id}">
+            <div class="product-bio">
+            <img src="${this.img}" alt="Some image" data-img="${this.img}">
+            <div class="product-desc">
+            <p class="product-title">${this.title}</p>
+           <p class="product-quantity">Quantity: ${this.quantity}</p>
+        <p class="product-single-price">$${this.price} </p>
+        </div>
+        </div>
+        <div class="right-block">
+             <p class="product-price">$${this.quantity * this.price}</p>
+             <button class="add-btn" data-id="${this.id}">+</button>
+            <button class="del-btn" data-id="${this.id}">-</button>
+        </div>
+        </div>`
+    }
+}
+let cardList = new CartList()
+let products = new ProductsList(cardList)
+
+
